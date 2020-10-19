867dbd1e9f017c34d550146f199f8839265d88c3
Konrad Mihelsson <konrad.mihelsson@gmail.com>
2020-10-19 19:08:26 +0300

    2. Уровень - Джуниор. Блок 2.3. SQL, JDBC. 4. JDBC. 1. Sql схема для проекта Tracker. [#302021]

---
+++ db/insert.sql
@@ -0,0 +1,6 @@
+insert into items (name) values('item1');
+insert into items (name) values('item2');
+insert into items (name) values('item3');
+insert into items (name) values('item4');
+insert into items (name) values('item5');
+insert into items (name) values('item6');
